b906455088cf1ad210bb0f0c140129843141dd62
katsu <sdjgsinhzoghaodg@yandex.ru>
2026-02-09 11:10:10 +0300

    upl

---
+++ notes.controller.js
@@ -0,0 +1,46 @@
+const fs = require('fs/promises');
+const path = require('path');
+const chalk = require('chalk');
+
+const notesPath = path.join(__dirname, 'db.json');
+
+async function addNote(title) {
+	const notes = await getNotes();
+	
+	const note = {
+		title,
+		id: Date.now().toString(),
+	}
+	
+	notes.push(note);
+	
+	await fs.writeFile(notesPath, JSON.stringify(notes));
+	console.log(chalk.bgGreen('Note added successfully.'));
+}
+
+async function printNotes() {
+	const notes = await getNotes();
+	
+	console.log(chalk.bgBlue('Here is the list of notes:'));
+	notes.forEach(note => {
+		console.log(chalk.blue(note.id + ' ' + note.title));
+	})
+}
+
+async function getNotes() {
+	const notes = await fs.readFile(notesPath, {encoding: 'utf8'});
+	return Array.isArray(JSON.parse(notes)) ? JSON.parse(notes) : [];
+}
+
+async function removeNote(id) {
+	const notes = await getNotes();
+	const filteredNotes = notes.filter(note => note.id !== id);
+	
+	await fs.writeFile(notesPath, JSON.stringify(filteredNotes));
+	
+	console.log(chalk.bgGreen('Note removed successfully.'));
+}
+
+module.exports = {
+	addNote, printNotes, removeNote
+}
